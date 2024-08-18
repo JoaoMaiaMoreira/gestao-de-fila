9099c956baa05f483785411f3c6e2418daf2defc
joao.moreira <joao.moreira@youxlab.com.br>
2024-08-18 15:53:08 -0300

    Arrumando a branch

---
+++ backend/gestao-filas/src/main/java/com/example/gestao_filas/GestaoFilasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestao_filas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFilasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFilasApplication.class, args);
+	}
+
+}
